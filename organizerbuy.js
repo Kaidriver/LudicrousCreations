6d1c10c193f85a7a3a2faa28f6c91e2e152bcd08
Kaiwen Song <jirachiji@gmail.com>
2020-12-21 16:44:16 -0500

    Added discounts and new favicon

--- organizerbuy.js
+++ organizerbuy.js
@@ -4,7 +4,7 @@ const designField = document.getElementById("select-design");
 const colorField = document.getElementById("select-color");
 const priceText = document.getElementById("price");
 const sizes = ["Short w/ 1 inch clamp", "Short w/ 2 inch clamp", "Long w/ 1 inch clamp", "Long w/ 2 inch clamp"];
-const prices = [15, 21, 21, 27];
+const prices = ["15.00 <sup class = 'discount text-success'><strike>$25.00</strike> 40% off</sup>", "21.00 <sup class = 'discount text-success'><strike>$35.00</strike> 40% off</sup>", "21.00 <sup class = 'discount text-success'><strike>$35.00</strike> 40% off</sup>", "27.00 <sup class = 'discount text-success'><strike>$45.00</strike> 40% off</sup>"];
 
 qtyField.addEventListener("change", function() {
   buyBtn.setAttribute("data-item-quantity", qtyField.value);
@@ -12,7 +12,7 @@ qtyField.addEventListener("change", function() {
 
 designField.addEventListener("change", function() {
   buyBtn.setAttribute("data-item-custom1-value", designField.value);
-  priceText.innerHTML = "$" + prices[sizes.indexOf(designField.value)] + ".00 Free Shipping";
+  priceText.innerHTML = "$" + prices[sizes.indexOf(designField.value)];
 })
 
 colorField.addEventListener("change", function() {
