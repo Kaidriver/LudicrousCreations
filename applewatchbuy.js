ef2540368fb42baca1641a235f7c5528bc326962
Kaiwen Song <jirachiji@gmail.com>
2021-04-26 20:55:10 -0400

    add cute apple watch stands

---
+++ applewatchbuy.js
@@ -0,0 +1,16 @@
+const buyBtn = document.querySelector(".buy-button");
+const qtyField = document.getElementById("quantity");
+const colorField = document.getElementById("select-color");
+const typeField = document.getElementById("select-type");
+
+qtyField.addEventListener("change", function() {
+  buyBtn.setAttribute("data-item-quantity", qtyField.value);
+});
+
+colorField.addEventListener("change", function() {
+  buyBtn.setAttribute("data-item-custom1-value", colorField.value);
+})
+
+typeField.addEventListener("change", function() {
+  buyBtn.setAttribute("data-item-custom2-value", typeField.value);
+})
